e396828478559a22ebc1bb1c46a4522e58709abb
Jun Ho Park <pjunho@umich.edu>
2020-11-23 14:34:10 -0800

    lock bugs

--- mock-sensor-tool.cpp
+++ mock-sensor-tool.cpp
@@ -209,7 +209,7 @@ void MockSensor::init()
                 std::lock_guard<std::mutex> configs_lock(_sensor_configs_mutex);
                 if (_sensor_configs.find(path) == _sensor_configs.end()) 
                 {
-                    _sensor_configs[path] = {false, false, true, 0, 0, ""};
+                    _sensor_configs[path] = {false, false, 0, 0, ""};
                     _paths.push_back(path);
                 }
 
@@ -246,43 +246,33 @@ void MockSensor::init()
                 else
                 {
                     // overload and return bytes read
-                    if (_sensor_configs[curr_path]._read_type)
+                    size_t val_length = _sensor_configs[curr_path]._overload_value.length();
+                    int padding = (val_length % sizeof(long) == 0) ? 0 : 
+                    ((val_length / sizeof(long)) + 1)*sizeof(long) - (val_length);
+
+                    char overload[val_length + padding];
+                    memcpy(overload, _sensor_configs[curr_path]._overload_value.data(), val_length);
+
+                    int upper_bound = (val_length + padding)/sizeof(long);
+
+                    for (size_t i = 0; i < padding; ++i)
                     {
-                        size_t val_length = _sensor_configs[curr_path]._overload_value.length();
-                        int padding = (val_length % sizeof(long) == 0) ? 0 : 
-                        ((val_length / sizeof(long)) + 1)*sizeof(long) - (val_length);
-
-                        char overload[val_length + padding];
-                        memcpy(overload, _sensor_configs[curr_path]._overload_value.data(), val_length);
-
-                        int upper_bound = (val_length + padding)/sizeof(long);
-
-                        for (size_t i = 0; i < padding; ++i)
-                        {
-                            overload[val_length + i] = '\0';
-                        }
-
-                        std::cout << "Injecting size " << (val_length + padding) << std::endl;
-                        for (size_t i = 0; i < upper_bound; ++i)
-                        {
-                            long new_sensor_val;
-                            memcpy(&new_sensor_val,
-                                overload + sizeof(long)*i,
-                                sizeof(long));
-
-                            std::cout << "Injecting word: " << std::hex << new_sensor_val << std::dec << std::endl;
-                            ptrace(PTRACE_POKEDATA, _pid, regs.uregs[1] + sizeof(long)*i, new_sensor_val);
-                        }
-                        
-                        ptrace(PTRACE_POKEUSER, _pid, 0, val_length);
-                    }
-                    // return EOF
-                    else
-                    {
-                        ptrace(PTRACE_POKEUSER, _pid, 0, 1);
+                        overload[val_length + i] = '\0';
                     }
 
-                    _sensor_configs[curr_path]._read_type = !_sensor_configs[curr_path]._read_type;
+                    std::cout << "Injecting size " << (val_length + padding) << std::endl;
+                    for (size_t i = 0; i < upper_bound; ++i)
+                    {
+                        long new_sensor_val;
+                        memcpy(&new_sensor_val,
+                            overload + sizeof(long)*i,
+                            sizeof(long));
+
+                        std::cout << "Injecting word: " << std::hex << new_sensor_val << std::dec << std::endl;
+                        ptrace(PTRACE_POKEDATA, _pid, regs.uregs[1] + sizeof(long)*i, new_sensor_val);
+                    }
+                    
+                    ptrace(PTRACE_POKEUSER, _pid, 0, val_length);
 
                     ptrace(static_cast<__ptrace_request>(PTRACE_GETREGS), _pid,
                        0, &regs);
